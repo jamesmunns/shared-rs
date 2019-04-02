d0bee2caa5b57fdc03391293f163f39b2b08a10c
James Munns <james.munns@ferrous-systems.com>
2019-04-02 02:50:11 +0200

    Ready for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,173 +1,155 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-use bare_metal::Nr;
-
-#[allow(dead_code)]
-enum IntStub {
-    Foo,
-    Bar,
-}
-
-unsafe impl Nr for IntStub {
-    fn nr(&self) -> u8 {
-        match *self {
-            IntStub::Foo => 3,
-            IntStub::Bar => 4,
-        }
-    }
-}
-
+#[macro_export]
 macro_rules! cmim {
     (
         $(($NAME:ident, $dat_ty:ty, $int:expr),)+
     ) => {
-        mod singletons {
-            $(
-                pub static mut $NAME: Option<$dat_ty> = None;
-            )+
-        }
+        pub use cmim::structs::*;
 
-        mod flags {
-            use ::core::sync::atomic::AtomicBool;
-            $(
-                pub static $NAME: AtomicBool = AtomicBool::new(false);
-            )+
-        }
+        pub mod cmim {
+            mod singletons {
+                $(
+                    pub static mut $NAME: Option<$dat_ty> = None;
+                )+
+            }
 
-        pub use structs::*;
-        mod structs {
-            use super::*;
-            use ::core::sync::atomic::Ordering;
-            use ::cortex_m::peripheral::NVIC;
+            mod flags {
+                use ::core::sync::atomic::AtomicBool;
+                $(
+                    pub static $NAME: AtomicBool = AtomicBool::new(false);
+                )+
+            }
 
-            $(
+            pub mod structs {
+                use ::core::sync::atomic::Ordering;
+                use ::cortex_m::peripheral::NVIC;
+                use ::bare_metal::Nr;
 
-                pub struct $NAME {
-                    _private: ()
-                }
+                // This is bad. I don't know how else to generically get
+                // the interrupt enum provided by the -PAC though.
+                use super::super::Interrupt;
 
-                impl $NAME {
-                    pub fn set_initial(data: $dat_ty) -> Result<$NAME, $dat_ty> {
-                        if int_is_enabled($int) || super::flags::$NAME.load(Ordering::SeqCst) {
-                            return Err(data);
-                        }
-
-                        if unsafe { super::singletons::$NAME.is_none() } {
-                            unsafe {
-                                super::singletons::$NAME = Some(data);
-                            }
-                            Ok($NAME { _private: () })
-                        } else {
-                            Err(data)
-                        }
+                $(
+                    pub struct $NAME {
+                        _private: ()
                     }
 
-                    pub fn modify_app_context<F>(&mut self, f: F) -> Result<(), ()>
-                    where
-                        for<'w> F: FnOnce(&'w mut $dat_ty) -> &'w mut $dat_ty,
-                    {
-                        // theoretical race condition: if an interrupt enables this interrupt between
-                        // the next line and the line after
-                        let enabled = int_is_enabled($int);
-                        if enabled {
-                            disable_int($int);
+                    impl $NAME {
+                        pub fn set_initial(data: $dat_ty) -> Result<$NAME, $dat_ty> {
+                            if int_is_enabled($int) || super::flags::$NAME.load(Ordering::SeqCst) {
+                                return Err(data);
+                            }
+
+                            if unsafe { super::singletons::$NAME.is_none() } {
+                                unsafe {
+                                    super::singletons::$NAME = Some(data);
+                                }
+                                Ok($NAME { _private: () })
+                            } else {
+                                Err(data)
+                            }
                         }
-                        if int_is_active($int) || unsafe { super::singletons::$NAME.is_none() } {
+
+                        pub fn modify_app_context<F>(&mut self, f: F) -> Result<(), ()>
+                        where
+                            for<'w> F: FnOnce(&'w mut $dat_ty) -> &'w mut $dat_ty,
+                        {
+                            // theoretical race condition: if an interrupt enables this interrupt between
+                            // the next line and the line after
+                            let enabled = int_is_enabled($int);
+                            if enabled {
+                                disable_int($int);
+                            }
+                            if int_is_active($int) || unsafe { super::singletons::$NAME.is_none() } {
+                                if enabled {
+                                    enable_int($int);
+                                }
+                                return Err(());
+                            }
+
+                            unsafe {
+                                f(super::singletons::$NAME.as_mut().unwrap());
+                            }
+
                             if enabled {
                                 enable_int($int);
                             }
-                            return Err(());
+
+                            Ok(())
                         }
 
-                        unsafe {
-                            f(super::singletons::$NAME.as_mut().unwrap());
-                        }
+                        pub fn modify_int_context<F>(f: F) -> Result<(), ()>
+                        where
+                            for<'w> F: FnOnce(&'w mut $dat_ty) -> &'w mut $dat_ty,
+                        {
+                            if !int_is_active($int) || unsafe { super::singletons::$NAME.is_none() } || super::flags::$NAME.swap(true, Ordering::SeqCst) {
+                                return Err(());
+                            }
 
-                        if enabled {
-                            enable_int($int);
-                        }
+                            unsafe {
+                                f(super::singletons::$NAME.as_mut().unwrap());
+                            }
 
-                        Ok(())
+                            assert!(super::flags::$NAME.swap(false, Ordering::SeqCst));
+                            Ok(())
+
+                        }
                     }
+                )+
 
-                    pub fn modify_int_context<F>(f: F) -> Result<(), ()>
-                    where
-                        for<'w> F: FnOnce(&'w mut $dat_ty) -> &'w mut $dat_ty,
-                    {
-                        if !int_is_active($int) || unsafe { super::singletons::$NAME.is_none() } || super::flags::$NAME.swap(true, Ordering::SeqCst) {
-                            return Err(());
-                        }
+                fn int_is_enabled<I>(interrupt: I) -> bool
+                    where I: Nr,
+                {
 
-                        unsafe {
-                            f(super::singletons::$NAME.as_mut().unwrap());
-                        }
+                    let nr = interrupt.nr();
+                    let mask = 1 << (nr % 32);
 
-                        assert!(super::flags::$NAME.swap(false, Ordering::SeqCst));
-                        Ok(())
-
-                    }
+                    // NOTE(unsafe) atomic read with no side effects
+                    unsafe { ((*NVIC::ptr()).iser[usize::from(nr / 32)].read() & mask) == mask }
                 }
-            )+
 
-            fn int_is_enabled<I>(interrupt: I) -> bool
-                where I: Nr,
-            {
+                fn int_is_active<I>(interrupt: I) -> bool
+                    where I: Nr
+                {
+                    let nr = interrupt.nr();
+                    let mask = 1 << (nr % 32);
 
-                let nr = interrupt.nr();
-                let mask = 1 << (nr % 32);
+                    // NOTE(unsafe) atomic read with no side effects
+                    unsafe { ((*NVIC::ptr()).iabr[usize::from(nr / 32)].read() & mask) == mask }
+                }
 
-                // NOTE(unsafe) atomic read with no side effects
-                unsafe { ((*NVIC::ptr()).iser[usize::from(nr / 32)].read() & mask) == mask }
-            }
+                fn disable_int<I>(interrupt: I)
+                    where I: Nr
+                {
+                    let nr = interrupt.nr();
 
-            fn int_is_active<I>(interrupt: I) -> bool
-                where I: Nr
-            {
-                let nr = interrupt.nr();
-                let mask = 1 << (nr % 32);
+                    unsafe { (*NVIC::ptr()).icer[usize::from(nr / 32)].write(1 << (nr % 32)) }
+                }
 
-                // NOTE(unsafe) atomic read with no side effects
-                unsafe { ((*NVIC::ptr()).iabr[usize::from(nr / 32)].read() & mask) == mask }
-            }
+                /// Enables `interrupt`
+                fn enable_int<I>(interrupt: I)
+                    where I: Nr
+                {
+                    let nr = interrupt.nr();
 
-            fn disable_int<I>(interrupt: I)
-                where I: Nr
-            {
-                let nr = interrupt.nr();
-
-                unsafe { (*NVIC::ptr()).icer[usize::from(nr / 32)].write(1 << (nr % 32)) }
-            }
-
-            /// Enables `interrupt`
-            fn enable_int<I>(interrupt: I)
-                where I: Nr
-            {
-                let nr = interrupt.nr();
-
-                unsafe { (*NVIC::ptr()).iser[usize::from(nr / 32)].write(1 << (nr % 32)) }
+                    unsafe { (*NVIC::ptr()).iser[usize::from(nr / 32)].write(1 << (nr % 32)) }
+                }
             }
         }
     }
 }
 
-cmim!(
-    (BAZ, usize, IntStub::Foo),
-    (BAX, usize, IntStub::Bar),
-);
+// cmim!(
+//     (BAZ, usize, Interrupt::Foo),
+//     (BAX, usize, Interrupt::Bar),
+// );
 
 // #[allow(dead_code)]
 // fn main() {
-//     let mut token = BAZ::$NAME::set_initial(27).unwrap();
+//     let mut token = BAZ::set_initial(27).unwrap();
 //     token.modify_app_context(|y| { *y -= 1; y }).unwrap();
 // }
 
 // #[allow(non_snake_case, dead_code)]
 // fn DEMO_INT() {
-//     BAZ::$NAME::modify_int_context(|x| { *x += 1; x }).unwrap();
+//     BAZ::modify_int_context(|x| { *x += 1; x }).unwrap();
 // }
